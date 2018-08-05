a1ded9734d9e1c198cf96d6506e50a631cbafce5
Linh Phan <linhphan0108@gmail.com>
2018-08-05 15:06:07 +0700

    - populate user info to drawer-nav-header

---
+++ app/src/main/java/com/example/linh/vietkitchen/ui/model/UserInfo.kt
@@ -0,0 +1,5 @@
+package com.example.linh.vietkitchen.ui.model
+
+import android.net.Uri
+
+data class UserInfo(val uid: String, val displayName: String?, val email: String?, val avatarUrl: Uri?)
--- app/src/main/java/com/example/linh/vietkitchen/ui/screen/home/homeActivity/DrawerNavHeaderLayoutDelegate.kt
+++ app/src/main/java/com/example/linh/vietkitchen/ui/screen/home/homeActivity/DrawerNavHeaderLayoutDelegate.kt
@@ -5,12 +5,13 @@ import android.view.ViewGroup
 import com.example.linh.vietkitchen.R
 import com.example.linh.vietkitchen.ui.model.DrawerNavHeader
 import com.example.linh.vietkitchen.ui.model.Entity
+import com.example.linh.vietkitchen.ui.model.UserInfo
 import com.hannesdorfmann.adapterdelegates3.AbsListItemAdapterDelegate
 
-class DrawerNavHeaderLayoutDelegate : AbsListItemAdapterDelegate<DrawerNavHeader, Entity, DrawerNavHeaderViewHolder>(){
+class DrawerNavHeaderLayoutDelegate(private val userInfo: UserInfo) : AbsListItemAdapterDelegate<DrawerNavHeader, Entity, DrawerNavHeaderViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup): DrawerNavHeaderViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_home_nav_header, parent, false)
-        return DrawerNavHeaderViewHolder(itemView)
+        return DrawerNavHeaderViewHolder(itemView, userInfo)
     }
 
     override fun isForViewType(item: Entity, items: MutableList<Entity>, position: Int) = item is DrawerNavHeader
