5319837ad2289929df3d39ac125976bf84dc7238
Linh Phan <linhphan0108@gmail.com>
2018-08-08 14:34:00 +0700

    make RecyclerView display sample data in preview window

---
+++ app/src/main/java/com/example/linh/vietkitchen/ui/custom/HotViewFlipper.kt
@@ -0,0 +1,30 @@
+package com.example.linh.vietkitchen.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ViewFlipper
+import com.example.linh.vietkitchen.R
+
+class HotViewFlipper : ViewFlipper {
+    private val initialView: Int
+
+    constructor(context: Context) : super(context) {
+        initialView = 0
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val styled = context.theme.obtainStyledAttributes(
+                attrs, R.styleable.HotViewFlipperAttrs, 0, 0)
+
+        initialView = try {
+            styled.getInteger( R.styleable.HotViewFlipperAttrs_initialView, 0)
+        } finally {
+            styled.recycle()
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        displayedChild = initialView % childCount
+    }
+}
