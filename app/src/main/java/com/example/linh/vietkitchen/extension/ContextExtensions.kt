51f77decfc6a1dd6831c6d9705115b18e4f2267b
Linh Phan <linhphan0108@gmail.com>
2018-08-10 09:32:34 +0700

    make snack bar show action

--- app/src/main/java/com/example/linh/vietkitchen/extension/ContextExtensions.kt
+++ app/src/main/java/com/example/linh/vietkitchen/extension/ContextExtensions.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Toast
+import com.example.linh.vietkitchen.R
 
 fun Context.color(res: Int): Int = ContextCompat.getColor(this, res)
 fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT){
@@ -16,20 +17,28 @@ fun Fragment.toast(message: String, length: Int = Toast.LENGTH_SHORT){
     context?.toast(message, length)
 }
 
-fun Context.showSnackBar(view: View, @StringRes resId: Int, length: Int = Snackbar.LENGTH_LONG, listener: View.OnClickListener? = null){
+fun Context.showSnackBar(view: View, @StringRes resId: Int, length: Int = Snackbar.LENGTH_LONG,
+                         action: String = getString(R.string.default_snack_bar_action),
+                         listener: View.OnClickListener? = null){
     Snackbar.make(view, resId, length)
-            .setAction("Action", listener).show()
+            .setAction(action, listener).show()
 }
 
-fun Context.showSnackBar(view: View, message: String, length: Int = Snackbar.LENGTH_LONG, listener: View.OnClickListener? = null){
+fun Context.showSnackBar(view: View, message: String, length: Int = Snackbar.LENGTH_LONG,
+                         action: String = getString(R.string.default_snack_bar_action),
+                         listener: View.OnClickListener? = null){
     Snackbar.make(view, message, length)
-            .setAction("Action", listener).show()
+            .setAction(action, listener).show()
 }
 
-fun Fragment.showSnackBar(view: View, @StringRes resId: Int, length: Int = Snackbar.LENGTH_LONG, listener: View.OnClickListener? = null){
-    context?.showSnackBar(view, resId, length, listener)
+fun Fragment.showSnackBar(view: View, @StringRes resId: Int, length: Int = Snackbar.LENGTH_LONG,
+                          action: String = getString(R.string.default_snack_bar_action),
+                          listener: View.OnClickListener? = null){
+    context?.showSnackBar(view, resId, length, action, listener)
 }
 
-fun Fragment.showSnackBar(view: View, message: String, length: Int = Snackbar.LENGTH_LONG, listener: View.OnClickListener? = null){
-    context?.showSnackBar(view, message, length, listener)
+fun Fragment.showSnackBar(view: View, message: String, length: Int = Snackbar.LENGTH_LONG,
+                          action: String = getString(R.string.default_snack_bar_action),
+                          listener: View.OnClickListener? = null){
+    context?.showSnackBar(view, message, length, action, listener)
 }
