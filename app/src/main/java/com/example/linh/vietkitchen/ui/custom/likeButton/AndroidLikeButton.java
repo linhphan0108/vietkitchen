a53b825743e3df4df69044c24f3e9ac5b253b7ad
Linh Phan <linhphan0108@gmail.com>
2018-11-09 15:27:47 +0700

    - make like button changeable - first item in home fragment is duplicated after it has liked

--- app/src/main/java/com/example/linh/vietkitchen/ui/custom/likeButton/AndroidLikeButton.java
+++ app/src/main/java/com/example/linh/vietkitchen/ui/custom/likeButton/AndroidLikeButton.java
@@ -284,7 +284,12 @@ public class AndroidLikeButton extends FrameLayout implements View.OnClickListen
         star.getLayoutParams().height = height;
         androidCircleView.setWidthAndHeight(width, height);
         androidDotView.setWidthAndHeight(width, height);
-        requestLayout();
+        post(new Runnable() {
+            @Override
+            public void run() {
+                requestLayout();
+            }
+        });
     }
 
     public void setLikeIcon(int resId) {
