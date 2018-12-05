4ba7fbe84edfd39d20733548b5480f09d9ae4636
Linh Phan <linhphan0108@gmail.com>
2018-12-05 15:59:06 +0700

    - disable shared elements transition because of crashing by openlg

--- app/src/main/java/com/example/linh/vietkitchen/ui/screen/home/BaseHomeFragment.kt
+++ app/src/main/java/com/example/linh/vietkitchen/ui/screen/home/BaseHomeFragment.kt
@@ -1,22 +1,16 @@
 package com.example.linh.vietkitchen.ui.screen.home
 
 import android.app.Activity
-import android.app.ActivityOptions
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.widget.TextView
-import com.example.linh.vietkitchen.R
 import com.example.linh.vietkitchen.ui.adapter.OnItemClickListener
 import com.example.linh.vietkitchen.ui.adapter.RecipeAdapter
 import com.example.linh.vietkitchen.ui.model.Recipe
 import com.example.linh.vietkitchen.ui.mvpBase.ToolbarFragment
 import com.example.linh.vietkitchen.ui.screen.detailActivity.BK_LIKE_STATE_JUST_CHANGED
 import com.example.linh.vietkitchen.ui.screen.detailActivity.RecipeDetailActivity
-import com.example.linh.vietkitchen.util.VerticalSpaceItemDecoration
 
 private const val REQUEST_DETAIL_RECIPE = 2
 abstract class BaseHomeFragment<V: BaseHomeContractView, P: BaseHomeContractPresenter<V>> : ToolbarFragment<V, P>(),
@@ -53,13 +47,13 @@ abstract class BaseHomeFragment<V: BaseHomeContractView, P: BaseHomeContractPres
     //recycler view callback
     override fun onItemClick(itemView: View, layoutPosition: Int, adapterPosition: Int, data: Recipe) {
         val intent = RecipeDetailActivity.createIntent(context, layoutPosition.toString(), data)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            itemView.transitionName = layoutPosition.toString()
-            val activityOptions = ActivityOptions.makeSceneTransitionAnimation(context as Activity, itemView, itemView.transitionName)
-            startActivityForResult(intent, REQUEST_DETAIL_RECIPE, activityOptions.toBundle())
-        }else{
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            itemView.transitionName = layoutPosition.toString()
+//            val activityOptions = ActivityOptions.makeSceneTransitionAnimation(context as Activity, itemView, itemView.transitionName)
+//            startActivityForResult(intent, REQUEST_DETAIL_RECIPE, activityOptions.toBundle())
+//        }else{
             startActivityForResult(intent, REQUEST_DETAIL_RECIPE)
-        }
+//        }
         lastItemClicked = adapterPosition
     }
 
